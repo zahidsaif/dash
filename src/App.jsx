8d0bd676fe0e9356bdd5b3d17446ea73417fdde1
Zahid Saif <zahid.18dbca@cmr.edu.in>
2022-09-02 09:38:37 +0530

    Initial layout update

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function App() {
+    return <>
+        <h2 className={"text-indigo-400 text-xl"}>TEST123</h2>
+        <p className={"text-center text-4xl"}>789</p>
+    </>
+}
+
+export default App;
